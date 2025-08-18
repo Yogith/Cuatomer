27e2ef0f93db5217e1b440fe3dc14c46b2d7677e
Yogith m <yogithn2122@gmail.com>
2025-08-18 10:38:00 +0000

    order

--- webapp/controller/customer.controller.js
+++ webapp/controller/customer.controller.js
@@ -9,32 +9,76 @@ sap.ui.define([
         onInit() {
         },
         
-        // formatDate: function (value) {
-        //     if (!value) return "";
-        //     const date = new Date(value);
-        //     return date.toLocaleDateString("en-GB"); // dd/mm/yyyy
+        formatDate: function (value) {
+            if (!value) return "";
+            const date = new Date(value);
+            return date.toLocaleDateString("en-GB"); // dd/mm/yyyy
+        },
+        progress: function (RequiredDate, ShippedDate) {
+            let oObjectStatus=this.byId("shippingstatus");
+            if (!ShippedDate) {
+                oObjectStatus.setState("Warning");
+                return "Pending Shipment";
+            }
+        
+            const reqDate = new Date(RequiredDate);
+            const shipDate = new Date(ShippedDate);
+        
+            if (shipDate <= reqDate) {
+                oObjectStatus.setState("Success");
+                return "In Time";
+            } else {
+                oObjectStatus.setState("Errorw");
+                return " Too Late";
+            }
+        },
+        // progresstate: function(RequiredDate, ShippedDate)
+        // {
+        //     if (!ShippedDate) {
+        //         return "Warning";
+        //     }
+        
+        //     const reqDate = new Date(RequiredDate);
+        //     const shipDate = new Date(ShippedDate);
+        
+        //     if (shipDate <= reqDate) {
+        //         return "Success";
+        //     } else {
+        //         return "Error";
+        //     }
+
         // },
 
         onSearch: function (oEvent) {
+            debugger
             let sValue = oEvent.getParameter("newValue");
             let aFilter=[];
             if (sValue) {
-                aFilter.push(new Filter("OrderID", FilterOperator.Contains, sValue));
-                // aFilter.push(
-                //     new Filter({
-                //         filters: [
-                //             new Filter("OrderID", FilterOperator.Contains, sValue),
-                //             // new Filter("ProductID", FilterOperator.Contains, sValue),
-                //             // new Filter("Discount", FilterOperator.Contains, sValue)
-                //         ],
-                //         and: false // OR condition
-                //     })
-                // );
+                aFilter.push(
+                    new Filter({
+                        filters: [
+                            //  new Filter("CustomerID", FilterOperator.contains, sValue),
+                             new Filter("ShipCountry", FilterOperator.contains, sValue)
+                            //  new Filter("OrderID", FilterOperator.contains, sValue)
+                        ],
+                        and: false // OR condition
+                    })
+                );
             }
 
-            const oList = this.byId("Order_DetailsList");
+            const oList = this.byId("Order");
             const oBinding = oList.getBinding("items");
             oBinding.filter(aFilter);
-        }
+        },
+        async onOpenDialog() {
+            this.oDialog ??= await this.loadFragment({
+                name: "customer.view.filterDialog"
+            });
+        
+            this.oDialog.open();
+        },
+        onCloseDialog() {
+			this.byId("helloDialog").close();
+		}
     });
 });
